237fb640340f0465c8c72c6bb8d77589a77a8a88
hugo.herrador <hugo.herrador@inetum.com>
2023-11-27 13:16:36 +0100

    set basic home comp

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>Hello world</p> `,
+  styles: ``
+})
+export default class HomeComponent {
+
+}
