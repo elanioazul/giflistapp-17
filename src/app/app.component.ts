9527c88457319b59f36bd86b76e5670f78e25e28
hugo.herrador <hugo.herrador@inetum.com>
2023-11-29 10:22:57 +0100

    style and error notification added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RedditService } from './shared/data-access/reddit.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,16 @@ import { RouterOutlet } from '@angular/router';
   styles: [],
 })
 export class AppComponent {
-  title = 'giflistapp-17';
+  redditService = inject(RedditService);
+  snackBar = inject(MatSnackBar);
+
+  constructor() {
+    effect(() => {
+      const error = this.redditService.error();
+
+      if (error !== null) {
+        this.snackBar.open(error, 'Dismiss', { duration: 2000 });
+      }
+    });
+  }
 }
--- src/app/home/ui/gif-list.component.ts
+++ src/app/home/ui/gif-list.component.ts
@@ -1,22 +1,63 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { Gif } from '../../shared/interfaces';
 import { GifPlayerComponent } from './gif-player.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { WINDOW } from '../../shared/utils/injection-tokens';
 @Component({
   selector: 'app-gif-list',
   standalone: true,
-  imports: [GifPlayerComponent],
+  imports: [
+    GifPlayerComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   template: `
     @for (gif of gifs; track gif.permalink){
     <div>
-    <app-gif-player
+      <app-gif-player
         [src]="gif.src"
         [thumbnail]="gif.thumbnail"
+        data-testid="gif-list-item"
       ></app-gif-player>
+      <mat-toolbar color="primary">
+        <span>{{ gif.title }}</span>
+        <span class="toolbar-spacer"></span>
+        <button
+          mat-icon-button
+          (click)="window.open('https://reddit.com/' + gif.permalink)"
+        >
+          <mat-icon>comment</mat-icon>
+        </button>
+      </mat-toolbar>
     </div>
+    } @empty {
+    <p>Can't find any gifs 🤷</p>
     }
   `,
-  styles: ``,
+  styles: [
+    `
+      div {
+        margin: 1rem;
+        filter: drop-shadow(0px 0px 6px #0e0c1ba8);
+      }
+
+      mat-toolbar {
+        white-space: break-spaces;
+      }
+
+      p {
+        font-size: 2em;
+        width: 100%;
+        text-align: center;
+        margin-top: 4rem;
+      }
+    `,
+  ],
 })
 export class GifListComponent {
   @Input({ required: true }) gifs!: Gif[];
+  window = inject(WINDOW);
 }
